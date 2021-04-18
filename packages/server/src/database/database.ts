6c451c446aa1bd05cda5ac27eb5556e3b69c9c30
DaiQiangReal <daiqianghaha@foxmail.com>
2021-04-18 17:39:26 +0800

    chore: backup

--- packages/server/src/database/database.ts
+++ packages/server/src/database/database.ts
@@ -2,76 +2,122 @@ import fsExtra from 'fs-extra'
 import path from 'path';
 import lodash from 'lodash';
 
-interface Operation{
-    method:'read'|'write';
-    target:string[]|string;
-    value?:string;
-    callback?:(data:string)=>any;
+interface Operation {
+    id?: string;
+    method: 'read' | 'write';
+    target: string[] | string;
+    value?: !Object;
+    callback?: (data: string) => any;
+
 }
 
-interface Config{
-    databasePath:string,
-    writeCacheSize?:number,
-    writeCacheTime?:number,
-    maxReadProcess?:number,
-    maxWriteProcess?:number,
+interface Config {
+    databasePath: string,
+    writeCacheSize?: number,
+    writeCacheTime?: number,
+    writeCacheDebounce?:number
 }
 
-class Database{
+class Database {
     private database: any;
-    private taskList: Promise<void>[];
-    private config:Config;
-    dirty: boolean;
-    constructor(config:Config) {
-        this.config={
-            writeCacheSize:10,
-            writeCacheTime:1000,
-            maxReadProcess:100,
-            maxWriteProcess:100,
+    private readTaskMap: Map<string, Promise<any>>;
+    private writeTaskMap: Map<string, Promise<any>>;
+    private config: Config;
+    private writeToDisk:Function;
+    constructor(config: Config) {
+        this.config = {
+            writeCacheSize: 10,
+            writeCacheTime: 1000,
+            writeCacheDebounce:1000,
             ...config
         }
         this.init(config.databasePath);
-        this.taskList=[];
-        this.dirty=false;
+        this.readTaskMap = new Map();
+        this.writeTaskMap = new Map();
+        this.writeToDisk=lodash.debounce(this._writeToDisk,this.config.writeCacheDebounce)
+        this.overrideMapSet();
     }
 
-    private init=(databasePath: string)=>{
-        try{
-            const database=fsExtra.readJSONSync(path.resolve(databasePath));
-            this.database=Database;
+    private init = (databasePath: string) => {
+        try {
+            const database = fsExtra.readJSONSync(path.resolve(databasePath));
+            this.database = Database;
             console.log('Loading database file success.');
 
-        }catch(e){
+        } catch (e) {
             throw `Error: failed to load database files, target path is ${databasePath} .\n\n ${e}`;
         }
     }
 
-    public do=(operation:Operation)=>{
-    
+    public do = (operation: Operation) => {
+        operation.id = `${new Date().getTime()}_${operation.method}_${operation.target}`;
+
     }
 
-    private createReadProcess=(target:string[]|string,callback:Function)=>{
-        const task=new Promise<void>((resolve)=>{
-            const data=lodash.get(this.database,target,null);
+    public read=async (target:string|string[])=>{
+        const id = `${new Date().getTime()}_read_${target}`;
+        return await new Promise((resolve)=>{
+            this.createReadProcess({id,method:'read',target,callback:resolve})
+        })
+    }
+
+    public write=async (target:string|string[],value:!Object)=>{
+        const id = `${new Date().getTime()}_write_${target}`;
+        this.createWriteProcess({id,method:'write',target,value})
+    }
+
+    private createReadProcess = (operation: Operation) => {
+        const { id, target, callback } = operation;
+        const task = new Promise<void>((resolve) => {
+            const data = lodash.get(this.database, target, null);
             callback(data);
             resolve();
-        });
-        this.taskList.push(task);
+        }).then(() => this.readTaskMap.delete(id));
+        this.readTaskMap.set(id, task);
+        return task;
     }
 
 
-    private createWriteProcess=(operation:Operation)=>{
-        const task=new Promise<void>((resolve)=>{
-            const {target,callback}=operation;
-            const value=lodash.get(operation,'value',null);
-            if(!value){
-                return;
+    private createWriteProcess = (operation: Operation) => {
+        const { id, target} = operation;
+        const task = new Promise<void>((resolve) => {
+            const value = lodash.get(operation, 'value', null);
+            if (!value) {
+                resolve();
             }
-            lodash.set(this.database,target,value);
+            lodash.set(this.database, target, value);
             resolve();
-        });
-        this.taskList.push(task);
+        }).then(() => this.writeTaskMap.delete(id));
+        this.writeTaskMap.set(id, task);
+        return task;
     }
 
-    
+    private overrideMapSet = () => {
+        const self = this;
+        const database = this.database;
+        let cacheSize = 0;
+        new Proxy(database, {
+            set: (target, key, value) => {
+                target[key] = value;
+                cacheSize++;
+                if (cacheSize > self.config.writeCacheSize) {
+                    cacheSize = 0;
+                    this.writeToDisk();
+                }
+                return true;
+            }
+        })
+    }
+
+    private _writeToDisk = async () => {
+        const { databasePath } = this.config;
+        await fsExtra.writeJSON(databasePath, this.database);
+    }
+
+
+
+    public exit = async () => {
+        await this._writeToDisk();
+    }
+
 }
