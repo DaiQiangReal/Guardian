4beb8a324614693fd072d4a3c19c8e0b9a09354c
DaiQiangReal <daiqianghaha@foxmail.com>
2021-05-02 20:08:55 +0800

    chore:backup

--- packages/web/src/pages/Main/index.tsx
+++ packages/web/src/pages/Main/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import {Button,Modal} from 'antd'
+import { Button, Modal } from 'antd'
 import styles from './index.module.scss'
-export interface MainProps { }
-
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export interface TestProps { compiler: string; }
+import ReactECharts from 'echarts-for-react';
+import SideNav from '../../components/SideNav'
 
 
-export default class Main extends React.Component<MainProps, {}> {
-    render() {
-        return <div >
-            <div className={styles['main']}>hellp</div>
-            <Button type="primary">test</Button>
-        </div>;
-    }
+
+export default () => {
+
+
+    return <div >
+        <SideNav/>
+       
+    </div>;
+
 }
